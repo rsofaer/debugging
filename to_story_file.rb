f8f0303e01a5e6a3b46f7ce328a12accc11e2ce8
Raphael Sofaer <rafi@sofaer.net>
2014-01-09 13:36:49 -0800

    files

---
+++ to_story_file.rb
@@ -0,0 +1,11 @@
+story_hash = {
+  "question" => "Welcome to the haunted hotel. What room would you like to go into?",
+  "room 1" => {"question" => "you're dead"},
+  "room 2" => { "question" => "You picked a good room. What would like to do?",
+    "sleep" => {"question" => "You're rested for the morning"},
+    "stay up all night" => {"question" => "You might have anxiety issues"}
+  }
+}
+def to_story_file(story_hash)
+
+end
